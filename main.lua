deb3f253455ba4c2a0d4d603fe26ac6f035e0eea
AlienatedSoftware <aidancgart@gmail.com>
2021-11-06 16:47:46 +0000

    Low res update

--- main.lua
+++ main.lua
@@ -1,16 +1,21 @@
 --[[
-    Window Size of game
+    Window Size of game with virtual low res for retro look
 ]]
+push = require 'push'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
---[[
-    Settings that runs once the game is booted up
-]]
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
+--[[
+    Settings that runs once the game is booted up - Texture scaling
+]]
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -18,14 +23,19 @@ function love.load()
 end
 
 --[[
-    Renders text on screen using LOVE 2D
+    Function to allow players to quit the game using the 'esc' key
 ]]
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
 
+--[[
+    Renders text on screen using LOVE 2D - Push functions ensures the text is rendered using virtual low res
+]]
 function love.draw()
-    love.graphics.printf(
-        'Hello World! Welcome to 1972.',
-        0,
-        WINDOW_HEIGHT / 2 - 6,
-        WINDOW_WIDTH,
-        'center')
+    push:apply('start')
+    love.graphics.printf('Hello World! Welcome to 1972.', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    push:apply('end')
 end
