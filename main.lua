3947cc045290fa04d0ea0a9ac3b3a217302ef2f6
AlienatedSoftware <aidancgart@gmail.com>
2021-11-06 18:32:44 +0000

    Rectangle update

--- main.lua
+++ main.lua
@@ -10,10 +10,13 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
 --[[
-    Settings that runs once the game is booted up - Texture scaling
+    Settings that runs once the game is booted up - Filter - Virtual Low Res - Font
 ]]
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -32,10 +35,22 @@ function love.keypressed(key)
 end
 
 --[[
-    Renders text on screen using LOVE 2D - Push functions ensures the text is rendered using virtual low res
+    Renders push.lua for virtual low res + rest of content commented
 ]]
 function love.draw()
     push:apply('start')
-    love.graphics.printf('Hello World! Welcome to 1972.', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- Clears colours and applies the true 1972 PONG colours
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    -- Renders text
+    love.graphics.printf('Hello World! Welcome to 1972.', 0, 20, VIRTUAL_WIDTH, 'center')
+
+    -- Left side player
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    -- Right side player
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    -- Ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
     push:apply('end')
 end
